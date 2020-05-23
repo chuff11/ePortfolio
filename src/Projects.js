a2bd660f36ee1e89900d6746f3991b832d4f3371
Cynthia Huffman <palin11@gmail.com>
2020-05-23 02:49:46 -0600

    Images, yay

--- src/Projects.js
+++ src/Projects.js
@@ -1,30 +1,34 @@
 import React from "react";
 import Thumbnail from './Thumbnail.js';
 import './App.css';
- 
+import cube from './assets/cube_thumbnail.png'
+import pyramid from './assets/pyramid_thumbnail.png'
+import ninja from './assets/ninja_thumbnail.png'
+
+
 function Projects(props) {
   return (
     <div className="project-container">
       <h1>Projects</h1>
         <Thumbnail
         link="/twitter"
-        image="http://twitter-image-url.jpg"
-        title="Twitter Newsfeed"
+        image= {cube}
+        title="Merge Cube Puzzle"
         category="Mobile App"
         />
         
         <Thumbnail
         link="/airbnb"
-        image="http://airbnb-image-url.jpg>"
-        title="Airbnb Experiences"
-        category="Website"
+        image={pyramid}
+        title="Pyramid Puzzle"
+        category="Mobile App"
         />
         
         <Thumbnail
         link="/photoshop"
-        image="http://photoshop-image-url.jpg"
-        title="Photoshop Redesign"
-        category="Desktop App"
+        image={ninja}
+        title="Ninja Burger"
+        category="Website"
         />
     </div>
   )
